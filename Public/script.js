402f565897ef189bb355471eb40513862312792f
TheAM01 <onevisionofficial55@gmail.com>
2024-03-20 01:12:07 +0500

    Final

--- Public/script.js
+++ Public/script.js
@@ -2,6 +2,7 @@ String.prototype.capitalizeInitial = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+
 function test () {
     console.log("Hello, Wilson!")
     setCookie("user_cookie_consent", "idk", 1)
